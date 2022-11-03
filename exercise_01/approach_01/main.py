981c15631c385ba9c96137191078364baeda16c5
Dmitry Kisler <admin@dkisler.com>
2022-11-03 16:34:56 +0100

    up
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>

--- exercise_01/approach_01/main.py
+++ exercise_01/approach_01/main.py
@@ -32,170 +32,240 @@ ORDER BY sum_amount DESC;
 
 - to send the result to stdout
 """
-from abc import abstractmethod
-from datetime import date
-from typing import Callable, Optional, Set, Type
-from uuid import UUID
-
-
-class FilteringError(Exception):
-    """Defines the error thrown when filtering error happened"""
+from typing import Type, Any, List, Dict, Tuple, Optional, Set
 
 
 class RowParsingError(Exception):
     """Defines the error thrown when a csv encoded row parsing error happened"""
 
 
-class Row:
-    """Table ``Row``"""
+# class Row:
+#     """Table ``Row``"""
+#
+#     @abstractmethod
+#     def parse_csv_row(self, row: str) -> None:
+#         """Parses csv encoded row.
+#
+#         Args:
+#             row (str): Row as csv encoded string.
+#         """
+#         pass
+#
+#     def __init__(self, raw_str: str) -> None:
+#         """Initialises table's ``Row``.
+#
+#         Args:
+#             raw_str (str): Row as csv encoded string.
+#
+#         Raises:
+#             RowParsingError: when csv encoded string parsing error happened.
+#         """
+#         try:
+#             self.parse_csv_row(raw_str)
+#         except Exception as ex:
+#             raise RowParsingError(ex) from ex
+#
+#
+# class RowUsers(Row):
+#     def parse_csv_row(self, row: str) -> None:
+#         raise NotImplemented("todo")
+#
+#     def __init__(self, raw_str: str):
+#         """Reads a user from csv encoded string if it's an active user."""
+#         self.user_id: UUID = UUID(bytes=bytes(0), version=4)
+#
+#         super().__init__(raw_str)
+#
+#
+# class RowTransactions(Row):
+#     def parse_csv_row(self, row: str) -> None:
+#         raise NotImplemented("todo")
+#
+#     def __init__(self, raw_str: str):
+#         """Reads a transaction from csv encoded string if it's not blocked."""
+#         self.transaction_id: UUID = UUID(bytes=bytes(0), version=4)
+#         self.user_id: UUID = UUID(bytes=bytes(0), version=4)
+#         self.transaction_amount: int = 0
+#         self.transaction_category_id: int = 0
+#
+#         super().__init__(raw_str)
 
-    @property
-    @abstractmethod
-    def header(self) -> Set[str]:
-        """Returns the header."""
-        pass
 
-    def apply_filter(self, filter_fn: Callable[["Row"], bool]) -> bool:
-        """Allies filter function.
+# @dataclass
+# class Column:
+#     """Defines table ``Column``"""
+#     name: str
+#     values: List[Any]
+#
+#     def sort(self, desc: bool = False) -> None:
+#         """Sort values.
+#
+#         Args:
+#             desc (bool): Sort in descending order.
+#         """
+#         self.values.sort(reverse=desc)
+
+Row = Tuple[Any]
+Column = List[Any]
+
+
+class Table:
+    """Defines column-oriented ``Table``"""
+
+    def __init__(self, columns: Tuple[Column], column_names: Optional[Set[str]]) -> None:
+        """Initiates a column-oriented ``Table``.
+
+        Note: columns are ordered and accessible by the order, not name.
 
         Args:
-            filter_fn (Callable): Filter function.
+            columns (tuple): Columns content.
+            column_names (set): Column names.
+
+        Raises:
+            ValueError: when column_names length does not match the ``Table`` size.
+        """
+        if column_names is not None:
+            if len(column_names) != len(columns):
+                raise ValueError("column_names does not match the table size")
+
+        self.columns: Tuple[Column] = columns
+        self.column_names: Set[str] = column_names
+        self.cnt_rows: int = len(self.columns[0])
+
+    def append_row(self, row: Row) -> None:
+        """Appends the ``Row``.
+
+        Args:
+            row (Row): Row to append to the table.
+
+        Raises:
+            KeyError: when row field corresponding to a column name is not found.
+        """
+        if len(row) != len(self.columns):
+            raise KeyError("row does not match the structure of the table")
+
+        for i, _ in enumerate(self.columns):
+            self.columns[i].append(row[i])
+
+    def read_row(self, index: int) -> Row:
+        """Returns the row at a given index.
+
+        Args:
+            index (int): Index of the row.
 
         Returns:
-            Flag: true if passes the filter, false otherwise.
+            The ``Row`` object.
 
         Raises:
-            FilteringError: when filtering error happened.
+            ValueError: when the index is not found.
         """
-        return filter_fn(self)
+        if index > self.cnt_rows or index < 0:
+            raise ValueError("index is not found")
 
-    @abstractmethod
-    def parse_csv_row(self, row: str) -> None:
-        """Parses csv encoded row.
+        return tuple(column[index] for column in self.columns)
+
+    def pop(self, column_index: int) -> Column:
+        """Removes column by its name and returns it
 
         Args:
-            row (str): Row as csv encoded string.
-        """
-        pass
+            column_index (int): Column index.
 
-    def __init__(self, raw_str: str) -> None:
-        """Initialises table's ``Row``.
-
-        Args:
-            raw_str (str): Row as csv encoded string.
+        Returns:
+            The ``Column`` object.
 
         Raises:
-            RowParsingError: when csv encoded string parsing error happened.
+            KeyError: when column is not found.
         """
-        try:
-            self.parse_csv_row(raw_str)
-        except Exception as ex:
-            raise RowParsingError(ex) from ex
+        if column_index < 0 or column_index > len(self.columns):
+            raise KeyError("column not found")
+
+        return self.columns[column_index]
+
+    def group_by(self, column_name: str) -> Dict[str, "Table"]:
+        """Groups the table by column.
+
+        Args:
+            column_name (str): Column name to be used to group.
+
+        Returns:
+            Dictionary with the key being the `column_name` and values tables subgroups.
+
+        Raises:
+            KeyError: when column is not found.
+        """
+        column_index = self.column_names
+        column_group_by: Column = self.pop(column_name)
+
+        result: Dict[str, "Table"]
+
+        for i, value in enumerate(column_group_by.values):
+            result[value] = None
+
+        return result
 
 
-Table = list[Row]
-Filter = Callable[[Type[Row]], Optional[Type[Row]]]
-
-
-class RowUsers(Row):
-    def parse_csv_row(self, row: str) -> None:
-        raise NotImplemented("todo")
-
-    @property
-    def header(self):
-        return {
-            "user_id",
-            "is_active",
-        }
-
-    @property
-    def id(self):
-        return self.user_id
-
-    def __init__(self, raw_str: str):
-        self.user_id: UUID = UUID(bytes=bytes(0), version=4)
-        self.is_active: bool = False
-
-        super().__init__(raw_str)
-
-
-class RowTransactions(Row):
-    def parse_csv_row(self, row: str) -> None:
-        raise NotImplemented("todo")
-
-    @property
-    def header(self):
-        return {
-            "transaction_id",
-            "date",
-            "user_id",
-            "is_blocked",
-            "transaction_amount",
-            "transaction_category_id",
-        }
-
-    @property
-    def id(self):
-        return self.transaction_id
-
-    def __init__(self, raw_str: str):
-        self.transaction_id: UUID = UUID(bytes=bytes(0), version=4)
-        self.date: date = date(1970, 1, 1)
-        self.user_id: UUID = UUID(bytes=bytes(0), version=4)
-        self.is_blocked: bool = False
-        self.transaction_amount: int = 0
-        self.transaction_category_id: int = 0
-        super().__init__(raw_str)
-
-
-class RowJoinResult(Row):
-    def parse_csv_row(self, row: str) -> None:
-        raise NotImplemented("todo")
-
-    @property
-    def header(self):
-        return {
-            "transaction_category_id",
-            "sum_amount",
-            "num_users",
-        }
-
-    @property
-    def id(self):
-        return self.transaction_category_id
-
-    def __init__(self, raw_str: str):
-        self.transaction_category_id: UUID = UUID(bytes=bytes(0), version=4)
-        self.sum_amount: int = 0
-        self.num_users: int = 0
-
-        super().__init__(raw_str)
-
-def read_and_filter(path: str, row_type: Type[Row], filter_fn: Filter) -> Table:
-    """Reads and filters data from a csv file.
+def read_users_csv(path: str) -> Table:
+    """Reads active users from csv file.
 
     Args:
-        path (str): Path to csv file.
-        row_type (Row): Row type.
-        filter_fn (Callable): Function to filter the input on row-by-row basis.
+        path (str): Path to `users.csv` file.
 
-    Returns:
-        Column oriented ``Table`` object.
+    Note:
+        The csv file must have the header and follow the structure "user_id,is_active".
+
+        The file
+
+        user_id,is_active
+        9f709688-326d-4834-8075-1a477d590af7,1
+        999eb541-c1a0-4888-aeb6-92773fc60e69,0
+        b923d15c-ce6d-4b2f-913f-31e87ebbcdc2,false
+        b1ee6da9-aca5-4bc6-bcfb-21ace2185055,true
+
+        corresponds to the table with two rows:
+
+        9f709688-326d-4834-8075-1a477d590af7
+        b1ee6da9-aca5-4bc6-bcfb-21ace2185055
     """
+    pass
+
+def read_transactions_csv(path: str) -> Table:
+    """Reads non-blocker transactions from csv file.
+
+    Args:
+        path (str): Path to `transactions.csv` file.
+
+    Note:
+        The csv file must have the header and follow
+            the structure "transaction_id,date,user_id,is_blocked,transaction_amount,transaction_category_id".
+
+        The file
+
+        transaction_id,date,user_id,is_blocked,transaction_amount,transaction_category_id
+        ce861100-26f0-4f1a-a8e3-8d6b3ad7a0e8,2022-01-01,9f709688-326d-4834-8075-1a477d590af7,1,100,1
+        3e6cdc49-f1c5-4ac6-9483-37622eed207a,2022-01-01,9f709688-326d-4834-8075-1a477d590af7,0,200,1
+        5c2e5c85-75e1-4137-bf13-529a000757f6,2022-02-01,b1ee6da9-aca5-4bc6-bcfb-21ace2185055,true,100,1
+        35715617-ea5d-4c00-842a-0aa81b224934,2022-02-02,b1ee6da9-aca5-4bc6-bcfb-21ace2185055,false,200,1
+        ca0d184e-7297-4ac2-95a6-6ed719a67b0a,2022-02-02,b1ee6da9-aca5-4bc6-bcfb-21ace2185055,false,20,2
+
+        would result in the table with one row:
+
+        3e6cdc49-f1c5-4ac6-9483-37622eed207a,9f709688-326d-4834-8075-1a477d590af7,200,1
+        35715617-ea5d-4c00-842a-0aa81b224934,b1ee6da9-aca5-4bc6-bcfb-21ace2185055,200,1
+        ca0d184e-7297-4ac2-95a6-6ed719a67b0a,b1ee6da9-aca5-4bc6-bcfb-21ace2185055,20,2
+    """
+    pass
 
 
-def main(path_users: str, filter_user: Optional[Filter], path_transactions: str, filter_transaction: Optional[Filter]):
+def main(path_users: str, path_transactions: str):
     """Application entrypoint.
 
     Args:
         path_users (str): Path to `users.csv` file.
-        filter_user (Callable): Function to filter users input on row-by-row basis.
         path_transactions (str): Path to `transactions.csv` file.
-        filter_transaction (Callable): Function to filter users input on row-by-row basis.
     """
 
-    data_users = read_and_filter(path_users, RowUsers, filter_user)
-    data_transactions = read_and_filter(path_transactions, RowUsers, filter_transaction)
+    users: Table = read_users_csv(path_users)
+    transactions: Table = read_transactions_csv(path_transactions)
 
     # join_map = join(data_transactions, data_users, ("user_id", "user_id"))
     # result = join_reduce(join_map)
@@ -203,4 +273,4 @@ def main(path_users: str, filter_user: Optional[Filter], path_transactions: str,
 
 
 if __name__ == "__main__":
-    main("users.csv", None, "transactions.csv", None)
+    main("users.csv", "transactions.csv")
