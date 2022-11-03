a929f80609428db7ae6e2067e074b3e244fb8cdf
Dmitry Kisler <admin@dkisler.com>
2022-11-03 23:09:21 +0100

    up
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>

--- exercise_01/approach_01/main.py
+++ exercise_01/approach_01/main.py
@@ -33,9 +33,11 @@ ORDER BY sum_amount DESC;
 - to send the result to stdout
 """
 import io
+import logging
 import os
 import sys
 from typing import Any, List, Dict, Tuple
+from uuid import UUID
 
 Column = List[Any]
 
@@ -183,11 +185,28 @@ class Table:
         return result
 
 
-def read_users_csv(path: str) -> Table:
+class RowParsingError(Exception):
+    ""
+
+
+def _to_bool(param: Any) -> bool:
+    """Helper function to convert to bool."""
+    if isinstance(param, int):
+        return int(param) > 0
+
+    if isinstance(param, str):
+        return param.lower() == "true"
+
+
+def read_users_csv(path: str, skip_header: bool = True) -> Table:
     """Reads active users from csv file.
 
     Args:
         path (str): Path to `users.csv` file.
+        skip_header (bool): Skip header row.
+
+    Returns:
+        ``Table`` object for users.
 
     Note:
         The csv file must have the header and follow the structure "user_id,is_active".
@@ -205,14 +224,42 @@ def read_users_csv(path: str) -> Table:
         9f709688-326d-4834-8075-1a477d590af7
         b1ee6da9-aca5-4bc6-bcfb-21ace2185055
     """
-    pass
+    result = Table(tuple(), column_names=tuple(["user_id"]))
+
+    cnt_row = 0
+    with open(path, "r") as f:
+        for line in f:
+            if cnt_row == 0 and skip_header:
+                continue
+
+            lst_columns: List[str] = line.rstrip().split(",")
+            if len(lst_columns) < 2:
+                raise RowParsingError("cannot parse the row %d" % cnt_row)
+
+            if not _to_bool(lst_columns[1]):
+                continue
+
+            try:
+                user_id = UUID(lst_columns[0])
+            except Exception as ex:
+                raise RowParsingError("failed to decode user_id for the row %d" % cnt_row) from ex
+
+            result.append_row(Row(tuple([user_id])))
+
+            cnt_row += 1
+
+    return result
 
 
-def read_transactions_csv(path: str) -> Table:
+def read_transactions_csv(path: str, skip_header: bool = True) -> Table:
     """Reads non-blocker transactions from csv file.
 
     Args:
         path (str): Path to `transactions.csv` file.
+        skip_header (bool): Skip header row.
+
+    Returns:
+        ``Table`` object for transactions.
 
     Note:
         The csv file must have the header and follow
@@ -233,7 +280,47 @@ def read_transactions_csv(path: str) -> Table:
         35715617-ea5d-4c00-842a-0aa81b224934,b1ee6da9-aca5-4bc6-bcfb-21ace2185055,200,1
         ca0d184e-7297-4ac2-95a6-6ed719a67b0a,b1ee6da9-aca5-4bc6-bcfb-21ace2185055,20,2
     """
-    pass
+    result = Table(tuple(),
+                   column_names=tuple(["transaction_id", "user_id", "transaction_amount", "transaction_category_id"]))
+
+    cnt_row = 0
+    with open(path, "r") as f:
+        for line in f:
+            if cnt_row == 0 and skip_header:
+                continue
+
+            lst_columns: List[str] = line.rstrip().split(",")
+            if len(lst_columns) < 2:
+                raise RowParsingError("cannot parse the row %d" % cnt_row)
+
+            if not _to_bool(lst_columns[3]):
+                continue
+
+            try:
+                transaction_id = UUID(lst_columns[0])
+            except Exception as ex:
+                raise RowParsingError("failed to decode transaction_id for the row %d" % cnt_row) from ex
+
+            try:
+                user_id = UUID(lst_columns[3])
+            except Exception as ex:
+                raise RowParsingError("failed to decode user_id for the row %d" % cnt_row) from ex
+
+            try:
+                transaction_amount = int(lst_columns[4])
+            except Exception:
+                raise RowParsingError("failed to decode transaction_amount for the row %d" % cnt_row)
+
+            try:
+                transaction_category_id = int(lst_columns[5])
+            except Exception:
+                raise RowParsingError("failed to decode transaction_category_id for the row %d" % cnt_row)
+
+            result.append_row(Row(tuple([transaction_id, user_id, transaction_amount, transaction_category_id])))
+
+            cnt_row += 1
+
+    return result
 
 
 def main(path_users: str, path_transactions: str):
@@ -243,28 +330,28 @@ def main(path_users: str, path_transactions: str):
         path_users (str): Path to `users.csv` file.
         path_transactions (str): Path to `transactions.csv` file.
     """
+    users: Table = read_users_csv(path_users)
+    transactions: Table = read_transactions_csv(path_transactions)
 
-    print(path_users)
-    print(path_transactions)
-    return
+    transactions_grouped_by_user = transactions.group_by("user_id")
 
-    # users: Table = read_users_csv(path_users)
-    # transactions: Table = read_transactions_csv(path_transactions)
-    #
-    # transactions_grouped_by_user = transactions.group_by("user_id")
-    #
-    # result_column_names = tuple(["transaction_category_id", "sum_amount", "num_users"])
-    #
-    # result = Table(columns=tuple([[]] * len(result_column_names)),
-    #                column_names=result_column_names)
-    #
-    # for user_id in set(transactions_grouped_by_user.keys()).intersection(set(users.column_by_name("user_id"))):
-    #     for i in range(len(transactions_grouped_by_user[user_id])):
-    #         transactions_grouped_by_user[user_id].read_row(i)
+    result_column_names = tuple(["transaction_category_id", "sum_amount", "num_users"])
+
+    result = Table(columns=tuple([[]] * len(result_column_names)),
+                   column_names=result_column_names)
+
+    for user_id in set(transactions_grouped_by_user.keys()).intersection(set(users.column_by_name("user_id"))):
+        for i in range(len(transactions_grouped_by_user[user_id])):
+            transactions_grouped_by_user[user_id].read_row(i)
 
 
 if __name__ == "__main__":
     path_users = os.getenv("PATH_USERS", "/data/users.csv")
     path_transactions = os.getenv("PATH_TRANSACTIONS", "/data/transactions.csv")
 
-    main(path_users, path_transactions)
+    logs = logging.Logger("join")
+
+    try:
+        main(path_users, path_transactions)
+    except Exception as ex:
+        logs.error(ex)
