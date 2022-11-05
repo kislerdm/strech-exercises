7e3621fa02064d80725ae29394df6263f693c655
Dmitry Kisler <admin@dkisler.com>
2022-11-05 15:43:09 +0100

    Add active users reader.
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>

--- exercise_01/solution_0/main.py
+++ exercise_01/solution_0/main.py
@@ -2,22 +2,16 @@
 
 import logging
 import os
+from typing import Callable, Optional
+from uuid import UUID
 
 
-def read_active_users(path: str, skip_header: bool = True) -> set[str]:
-    """Reads active users from `users.csv` file.
-
-    Args:
-        path (str): Path to `users.csv` file.
-        skip_header (bool): Skip csv header.
-
-    Returns:
-        Set of user ID.
-    """
+def parse_row(row: list[str], converter: Optional[Callable[[list[str]], tuple[any, ...]]]) -> tuple[any, ...]:
+    """active users from `users.csv` file"""
     pass
 
 
-class Reader:
+class CSVReader:
     """Reads the csv file."""
 
     def _open(self) -> None:
@@ -31,10 +25,9 @@ class Reader:
         self.header_skipped = not skip_header
 
     def __iter__(self):
-
         return self
 
-    def __next__(self):
+    def __next__(self) -> Optional[list[str]]:
         self._readline()
         o = self.line
 
@@ -42,7 +35,7 @@ class Reader:
             self.header_skipped = not self.header_skipped
             return next(self)
 
-        return o
+        return o.split(",")
 
     def _readline(self):
         try:
@@ -52,9 +45,39 @@ class Reader:
             self._file_io.close()
             raise e
 
-    def reopen(self):
-        self._file_io.close()
-        self._open()
+
+def _is_true(s: str) -> bool:
+    """Helper function to check if True."""
+    return s.lower() == "true" or s == "1"
+
+
+class DataQualityError(Exception):
+    """Error raised if data are not valid."""
+    pass
+
+
+def read_active_users(reader: CSVReader) -> set[UUID]:
+    """Reads active users from the `users.csv`.
+
+    Args:
+        reader (CSVReader): Initialised CSVReader object.
+
+    Returns:
+        Set of active user ID.
+    """
+    o: set[UUID] = set()
+
+    for cols in reader:
+        if len(cols) < 2:
+            raise DataQualityError("wrong number of columns")
+
+        if _is_true(cols[1]):
+            try:
+                o.add(UUID(cols[0]))
+            except Exception:
+                raise DataQualityError("failed to decode user_id")
+
+    return o
 
 
 def main(path_users: str, path_transactions: str, skip_header: bool = True) -> None:
@@ -77,7 +100,7 @@ def main(path_users: str, path_transactions: str, skip_header: bool = True) -> N
             GROUP BY t.transaction_category_id
             ORDER BY sum_amount DESC;
     """
-    active_users: set[str] = read_active_users(path_users, skip_header)
+    active_users: set[UUID] = read_active_users(CSVReader(path_users, skip_header))
 
 
 if __name__ == "__main__":
--- exercise_01/solution_0/test_main.py
+++ exercise_01/solution_0/test_main.py
@@ -1,22 +1,25 @@
+from typing import Optional
+from uuid import UUID
+
 import pytest
 
-from main import Reader
+from main import CSVReader, read_active_users, DataQualityError
 
 
 @pytest.mark.parametrize("file_content,reads_count,skip_header,want,is_eof", [
-    ("""foo,bar\n1,\"a\"\n""", 1, True, "1,\"a\"", False),
-    ("""foo,bar\n1,\"a\"\n""", 1, False, "foo,bar", False),
-    ("""foo,bar\n1,\"a\"\n""", 2, True, "", True),
+    ("""foo,bar\n1,a\n""", 1, True, ["1", "a"], False),
+    ("""foo,bar\n1,a\n""", 1, False, ["foo", "bar"], False),
+    ("""foo,bar\n1,a\n""", 2, True, None, True),
     ("", 1, False, "", True),
 ])
 class TestReader:
     def test_reader_single_row(self, mocker, reads_count, skip_header, file_content, want, is_eof):
         mocker.patch("builtins.open", mocker.mock_open(read_data=file_content))
-        reader = Reader("foo.csv", skip_header)
+        reader = CSVReader("foo.csv", skip_header)
 
         assert reader.header_skipped is not skip_header, "faulty header status"
 
-        got: str = ""
+        got: Optional[list[str]] = None
 
         try:
             for _ in range(reads_count):
@@ -25,3 +28,56 @@ class TestReader:
         except StopIteration:
             if is_eof:
                 pass
+
+
+@pytest.mark.parametrize("file_content,want,is_error,error_msg", [
+    (
+            """user_id,is_active
+    9f709688-326d-4834-8075-1a477d590af7,1
+    999eb541-c1a0-4888-aeb6-92773fc60e69,0
+    b923d15c-ce6d-4b2f-913f-31e87ebbcdc2,false
+    b1ee6da9-aca5-4bc6-bcfb-21ace2185055,true
+    """,
+            {UUID("9f709688-326d-4834-8075-1a477d590af7"), UUID("b1ee6da9-aca5-4bc6-bcfb-21ace2185055")},
+            False,
+            "",
+    ),
+    (
+            """user_id,is_active
+    9f709688-326d-4834-8075-1a477d590af7,0
+    999eb541-c1a0-4888-aeb6-92773fc60e69,0
+    b923d15c-ce6d-4b2f-913f-31e87ebbcdc2,false
+    b1ee6da9-aca5-4bc6-bcfb-21ace2185055,false
+    """,
+            {},
+            False,
+            "",
+    ),
+    (
+            """user_id,is_active
+    9f709688-326d-4834-8075-1a477d590af7
+    """,
+            {},
+            True,
+            "wrong number of columns",
+    ),
+    (
+            """user_id,is_active
+    1,1
+    """,
+            {},
+            True,
+            "failed to decode user_id",
+    ),
+])
+def test_read_active_users(mocker, file_content, want, is_error, error_msg):
+    mocker.patch("builtins.open", mocker.mock_open(read_data=file_content))
+
+    reader = CSVReader("foo.csv", skip_header=True)
+
+    try:
+        got = read_active_users(reader)
+        assert got == want
+    except DataQualityError as ex:
+        if is_error and ex.__str__() == error_msg:
+            pass
