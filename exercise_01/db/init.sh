41b0d3582cda19fd6751b73c55d939185bd9062e
Dmitry Kisler <admin@dkisler.com>
2022-11-10 00:39:41 +0100

    Fix linting
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>

--- exercise_01/db/init.sh
+++ exercise_01/db/init.sh
@@ -9,11 +9,11 @@ readonly REQUIRED_ENV_VARS=(
 )
 
 check_env_vars_set() {
-  for required_env_var in ${REQUIRED_ENV_VARS[@]}; do
+  for required_env_var in "${REQUIRED_ENV_VARS[@]}"; do
     if [[ -z "${!required_env_var}" ]]; then
       echo "Error: environment variable '${required_env_var}' not set.
       Make sure you have the following environment variables set:
-      ${REQUIRED_ENV_VARS[@]}
+      ${REQUIRED_ENV_VARS[*]}
       Aborting."
       exit 1
     fi
